a54cbc55e17613b36daa0edb5fb35aeee2b4b0f2
Jonas Karg <jonas.karg@hotmail.com>
2019-08-08 15:28:49 +0200

    New interpreter.

--- interpreter.js
+++ interpreter.js
@@ -2,10 +2,29 @@ function replaceCharAt(str, index, char) {
   return `${ str.substr(0, index) }${ char }${ str.substr(index + 1) }`;
 }
 
-function runProgram(input) {
+function validatePointer(pointer, memorySize) {
+  if (0 <= pointer && pointer < memorySize) return pointer;
+  const error = new Error(`Pointer 0x${pointer.toString(16)} outside of address range (0x0 - 0x${(memorySize - 1).toString(16)}).`);
+  error.name = "Memory Error";
+  throw error;
+}
+
+function runProgram(input, outputBuffer, inputBuffer, memorySize = 128) {
+  if (memorySize < 1) throw new Error(`Invalid memory size: ${memorySize}`);
+  if (!(outputBuffer instanceof Array)) console.warn("No output buffer provided.");
+  if (!(inputBuffer instanceof Array)) console.warn("No input buffer provided.");
+
+  const memory = new Int32Array(memorySize).fill(0);
+  const rawTokens = input.split("");
+  const tokens = [];
+
+  let pointer = programState = column = 0;
+  let rawInstructionList = "";
+  let line = 1;
+
   const instructionLookup = {
-    ">": () => pointer++,
-    "<": () => pointer--,
+    ">": () => validatePointer(++pointer, memorySize),
+    "<": () => validatePointer(--pointer, memorySize),
     "+": () => memory[pointer]++,
     "-": () => memory[pointer]--,
     ".": () => outputBuffer.push(memory[pointer]),
@@ -14,22 +33,14 @@ function runProgram(input) {
     "]": () => memory[pointer] !== 0 ? (programState = tokens[programState].jump) : 0
   };
 
-  const memory = new Int32Array(128).fill(0);
-  const inputBuffer = new Int32Array();
-  const outputBuffer = new Int32Array();
-  const rawTokens = input.split("");
-  const tokens = [];
-
-  let rawInstructionList = "";
-  let line = column = 1;
-  let pointer = programState = 0;
-
   for (const token of rawTokens) {
     if (token === "\n") {
       line++;
-      column = 1;
+      column = 0;
       continue;
     }
+
+    column++;
 
     const instruction = instructionLookup[token];
     if (!instruction) continue;
@@ -48,16 +59,25 @@ function runProgram(input) {
     // Matching two braces.
     const match = rawInstructionList.match(/\[[^\[\]]*\]/);
 
-    // Error/Finish checking.
     if (!match) {
-      const hasOpenBrace = rawInstructionList.includes("[");
-      const hasCloseBrace = rawInstructionList.includes("]");
-      if (hasOpenBrace && !hasCloseBrace) throw new Error("Brace opened but not closed");
-      if (hasCloseBrace && !hasOpenBrace) throw new Error("Brace closed but not opened");
+      const openBraceIndex = rawInstructionList.indexOf("[");
+      const closeBraceIndex = rawInstructionList.indexOf("]");
+      const hasOpenBrace = openBraceIndex > -1;
+      const hasCloseBrace = closeBraceIndex > -1;
+
+      if (hasOpenBrace && !hasCloseBrace) {
+        const token = tokens[openBraceIndex];
+        throw new Error(`Brace opened but not closed @ ${ token.line }:${ token.column }`);
+      }
+
+      if (hasCloseBrace && !hasOpenBrace) {
+        const token = tokens[closeBraceIndex];
+        throw new Error(`Brace closed but not opened @ ${ token.line }:${ token.column }`);
+      }
+
       break;
     }
 
-    // The indices of the opening and closing braces.
     const start = match.index;
     const end = start + match[0].length - 1;
 
@@ -70,6 +90,7 @@ function runProgram(input) {
     rawInstructionList = replaceCharAt(rawInstructionList, end, 0);
   }
 
+  // Running the instruction of each token
   for (programState = 0; programState < tokens.length; programState++) {
     tokens[programState].instruction();
   }
--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-const testProgram =
-  "++++++++[>++aaa++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+async function run() {
+  const testProgram = await fetch("program.bf")
+    .then(res => res.text());
 
-console.log(runProgram(testProgram));
+  const inputBuffer = [84, 101, 115, 116];
+  const outputBuffer = [];
+
+  runProgram(testProgram, outputBuffer, inputBuffer, 7);
+
+  const ascii = outputBuffer.map(char => String.fromCharCode(char)).join("");
+
+  console.log(ascii);
+}
+
+run();
