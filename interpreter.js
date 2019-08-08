300de03fd8dff1ff3fde2003ac35d740af883229
Jonas Karg <jonas.karg@hotmail.com>
2019-08-08 12:19:24 +0200

    Rework

--- interpreter.js
+++ interpreter.js
@@ -1,56 +1,78 @@
-const mem = new Int32Array(128).fill(0);
-let pointer = 0;
-let inputBuffer = [];
-
-const testProgram =
-  "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
-
 function replaceCharAt(str, index, char) {
-  return `${str.substr(0, index)}${char}${str.substr(index + 1)}`;
+  return `${ str.substr(0, index) }${ char }${ str.substr(index + 1) }`;
 }
 
-function compile(input) {
-  const jumps = {};
-  let tmp = input;
+function runProgram(input) {
+  const instructionLookup = {
+    ">": () => pointer++,
+    "<": () => pointer--,
+    "+": () => memory[pointer]++,
+    "-": () => memory[pointer]--,
+    ".": () => outputBuffer.push(memory[pointer]),
+    ",": () => memory[pointer] = inputBuffer.shift() | 0,
+    "[": () => memory[pointer] === 0 ? (programState = tokens[programState].jump + 1) : 0,
+    "]": () => memory[pointer] !== 0 ? (programState = tokens[programState].jump) : 0
+  };
+
+  const memory = new Int32Array(128).fill(0);
+  const inputBuffer = new Int32Array();
+  const outputBuffer = new Int32Array();
+  const rawTokens = input.split("");
+  const tokens = [];
+
+  let rawInstructionList = "";
+  let line = column = 1;
+  let pointer = programState = 0;
+
+  for (const token of rawTokens) {
+    if (token === "\n") {
+      line++;
+      column = 1;
+      continue;
+    }
+
+    const instruction = instructionLookup[token];
+    if (!instruction) continue;
+    rawInstructionList += token;
+
+    tokens.push({
+      instruction,
+      line,
+      column,
+      raw: token,
+      jump: null
+    });
+  }
 
   while (true) {
-    const match = tmp.match(/\[[^\[\]]*\]/);
+    // Matching two braces.
+    const match = rawInstructionList.match(/\[[^\[\]]*\]/);
 
+    // Error/Finish checking.
     if (!match) {
-      const hasOpenBrace = tmp.includes("[");
-      const hasCloseBrace = tmp.includes("]");
+      const hasOpenBrace = rawInstructionList.includes("[");
+      const hasCloseBrace = rawInstructionList.includes("]");
       if (hasOpenBrace && !hasCloseBrace) throw new Error("Brace opened but not closed");
       if (hasCloseBrace && !hasOpenBrace) throw new Error("Brace closed but not opened");
       break;
     }
 
+    // The indices of the opening and closing braces.
     const start = match.index;
     const end = start + match[0].length - 1;
 
-    jumps[start] = end;
-    tmp = replaceCharAt(tmp, start, "x");
-    tmp = replaceCharAt(tmp, end, "x");
+    // Adding information to "[" and "]" tokens on where their counterpart is.
+    tokens[start].jump = end;
+    tokens[end].jump = start;
+
+    // Removing the matched tokens to prevent matching them agin.
+    rawInstructionList = replaceCharAt(rawInstructionList, start, 0);
+    rawInstructionList = replaceCharAt(rawInstructionList, end, 0);
   }
 
-  Object.entries(jumps).map(([open, close]) => (jumps[close] = open | 0));
-
-  let programState;
-
-  const lookup = {
-    ">": () => pointer++,
-    "<": () => pointer--,
-    "+": () => mem[pointer]++,
-    "-": () => mem[pointer]--,
-    ".": () => console.log(mem[pointer]),
-    ",": () => mem[pointer] = inputBuffer.shift() | 0,
-    "[": () => mem[pointer] === 0 ? programState = jumps[programState] + 1 : 0,
-    "]": () => mem[pointer] !== 0 ? programState = jumps[programState] : 0,
-  };
-
-  for (programState = 0; programState < input.length; programState++) {
-    const command = lookup[input[programState]];
-    if (command) command();
+  for (programState = 0; programState < tokens.length; programState++) {
+    tokens[programState].instruction();
   }
+
+  return outputBuffer;
 }
-
-compile(testProgram);
